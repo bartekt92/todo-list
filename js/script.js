5f2e7322eb05b34aefe5a0abe3f4f8a37e9ab35f
Barłomiej Tabero <bartekt92@gmail.com>
2021-05-30 23:31:25 +0200

    working version of ready program

--- js/script.js
+++ js/script.js
@@ -1,28 +1,44 @@
 {
+    let doneHide = false;
+    let disabled = false;
+
     const welcome = () => {
         console.log("Witajcie programiści!")
     }
-    const tasks = [
+    let tasks = [
 
     ];
 
     const addNewTask = (newTaskContent) => {
-        tasks.push({
-            content: newTaskContent,
-        });
+        tasks = [
+            ...tasks,
+            { content: newTaskContent },
+        ];
         render();
     };
 
 
     const removeTask = (taskIndex) => {
-        tasks.splice(taskIndex, 1);
+        tasks = [
+            ...tasks.slice(0, taskIndex),
+            ...tasks.slice(taskIndex + 1),
+        ];
         render();
     };
 
-    const doneTask = (taskIndex) => {
-        tasks[taskIndex].done = !tasks[taskIndex].done;
+    const toggleDoneTask = (taskIndex) => {
+        const task = tasks[taskIndex];
+        tasks = [
+            ...tasks.slice(0, taskIndex),
+            {
+                ...task,
+                done: !task.done,
+            },
+            ...tasks.slice(taskIndex + 1),
+        ];
         render();
     };
+
     const bindEvents = () => {
 
         const removeButtons = document.querySelectorAll(".js-eraseButton");
@@ -34,30 +50,75 @@
         const doneButtons = document.querySelectorAll(".js-doneButton");
         doneButtons.forEach((doneButton, taskIndex) => {
             doneButton.addEventListener("click", () => {
-                doneTask(taskIndex);
+                toggleDoneTask(taskIndex);
             });
         });
     }
 
+
+    const renderNewButtons = () => {
+        const addNewButtons = document.querySelector(".js-addNewButtons");
+        if (tasks.length) {
+            addNewButtons.innerHTML = `<h2 class="section__header section__header--second">Lista zadań</h2>
+     <button class="section__whiteButton js-showDoneTasksButton">${doneHide ? "Pokaż ukończone" : "Ukryj ukończone"}</button>
+     <button ${tasks.every(({ done }) => done) ? "disabled" : ""} class="section__whiteButton js-doneAllTasksButton">Ukończ wszystkie</button>`;
+            bindNewButtons();
+        } else if (!tasks.length) {
+            addNewButtons.innerHTML = `<h2 class="section__header section__header--second">Lista zadań</h2>`
+        };
+    };
+
+
+    const toggleDoneAllTasks = () => {
+        tasks = tasks.map((task) => ({
+            ...task,
+            done: true,
+        }));
+        render();
+    };
+
+
+
+    const bindNewButtons = () => {
+        const allTasksDoneButton = document.querySelector(".js-doneAllTasksButton");
+
+        allTasksDoneButton.addEventListener("click", () => {
+            toggleDoneAllTasks();
+        });
+
+        const hideButton = document.querySelector(".js-showDoneTasksButton");
+        hideButton.addEventListener("click", () => {
+            doneHide = !doneHide;
+            render();
+        });
+    };
+
+
+
+
+
     const render = () => {
         let htmlString = "";
         for (const task of tasks) {
-            htmlString += ` <li class="section__list">
+            htmlString += ` <li class="section__list js-list ${doneHide && task.done ? "section__list--hide" : ""}">
             <button class="section__actionButton js-doneButton">${task.done ? "&#10004;" : ""}</button>
         <span class="section__text ${task.done ? "section__text--done" : ""}">${task.content}</span>
         <button class="section__actionButton section__actionButton--red js-eraseButton">&#128465;</button>
     </li>
-    <hr class="section__line">
+    <hr class="section__line ${doneHide && task.done ? "section__line--hide" : ""}">
            `
         }
         document.querySelector(".js-tasks").innerHTML = htmlString;
         bindEvents();
+        renderNewButtons();
     };
 
 
     const onFormSubmit = (event) => {
         event.preventDefault();
         document.querySelector(".js-newTask").focus();
+
+
         const newTaskContent = document.querySelector(".js-newTask").value.trim();
         if (newTaskContent === "") {
             return;
@@ -73,5 +134,4 @@
         render();
     }
     init();
-
 }
